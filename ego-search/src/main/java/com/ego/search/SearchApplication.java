dbe9d8bc32f17d53d68c99bd17f161de31a4fbb5
your_username <1298065072@qq.com>
2020-05-09 14:02:05 +0800

    配置了rpc 和search

--- ego-search/src/main/java/com/ego/search/SearchApplication.java
+++ ego-search/src/main/java/com/ego/search/SearchApplication.java
@@ -1,20 +1,15 @@
 package com.ego.search;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 启动类
- */
 @SpringBootApplication
-//扫描mapper接口
 @MapperScan("com.ego.search.mapper")
-//dubbo配置
-@EnableDubboConfig
+@EnableDubboConfiguration
 public class SearchApplication {
-    public static void main(String[] args) {
-            SpringApplication.run(SearchApplication.class,args);
+    public static void main(String[] args){
+        SpringApplication.run(SearchApplication.class,args);
     }
 }
